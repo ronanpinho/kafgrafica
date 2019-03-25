2ddc0c7275dca2e2564c657f7f99264e478442ba
Jimmy Kedlyn <jimmykedlyn@gmail.com>
2019-03-25 16:38:32 -0300

    Começo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,3 +14,18 @@ $(function() {
         });
     });
 });
+
+setInterval(function time(){
+    var d = new Date();
+    var days = 2 - d.getDay();
+    var hours = 24 - d.getHours();
+    var min = 60 - d.getMinutes();
+    if((min + '').length == 1){
+        min = '0' + min;
+    }
+    var sec = 60 - d.getSeconds();
+    if((sec + '').length == 1){
+        sec = '0' + sec;
+    }
+    jQuery('#week-countdown p').html(days+'d : '+hours+'h : '+min+'m : '+sec+'s ')
+}, 1000);
